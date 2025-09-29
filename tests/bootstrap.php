335b35166fd4fb2ccf38011875c1363254f9926d
Slavko Hinkin <slavihh@gmail.com>
2025-09-29 17:09:08 +0300

    fix for codestyle

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,16 +12,15 @@ if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
 
-$kernel = new \App\Kernel('test', true);
+$kernel = new App\Kernel('test', true);
 
 $kernel->boot();
 
-$application = new \Symfony\Bundle\FrameworkBundle\Console\Application($kernel);
+$application = new Symfony\Bundle\FrameworkBundle\Console\Application($kernel);
 $application->setAutoExit(false);
 
-passthru(\sprintf('APP_ENV=%s php "%s/../bin/console" doctrine:database:drop --force --if-exists', $_ENV['APP_ENV'], __DIR__));
-passthru(\sprintf('APP_ENV=%s php "%s/../bin/console" doctrine:database:create', $_ENV['APP_ENV'], __DIR__));
-passthru(\sprintf('APP_ENV=%s php "%s/../bin/console" doctrine:migrations:migrate --no-interaction', $_ENV['APP_ENV'], __DIR__));
-
+passthru(sprintf('APP_ENV=%s php "%s/../bin/console" doctrine:database:drop --force --if-exists', $_ENV['APP_ENV'], __DIR__));
+passthru(sprintf('APP_ENV=%s php "%s/../bin/console" doctrine:database:create', $_ENV['APP_ENV'], __DIR__));
+passthru(sprintf('APP_ENV=%s php "%s/../bin/console" doctrine:migrations:migrate --no-interaction', $_ENV['APP_ENV'], __DIR__));
 
 $kernel->shutdown();
